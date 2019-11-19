00ccec214e5e014734ef062c925619b20459a6e1
Spurious Dr. Stein <35902579+SpuriousDrStein@users.noreply.github.com>
2019-11-19 03:13:11 +0100

    Update structure.jl

--- structure.jl
+++ structure.jl
@@ -19,9 +19,10 @@ mutable struct Edge
     thr::FloatN
 end
 mutable struct ResidualGraph
+    inital_edges::Vector{Edge}
     edges::Vector{Edge}
-    internal_vertecies::Array{Vertex, 2}
-    external_vertecies::Array{Vertex, 2}
+    internal_vertecies::Array{Union{Vertex, Missing}, 2}
+    external_vertecies::Array{Array{Union{Vertex, Missing}, 2}} # for each layer individually
     L::Integer
 end
 mutable struct Layer
@@ -31,52 +32,87 @@ mutable struct Layer
     L::Integer
 end
 
-random_vertex(init_dropout=0.5) = rand() >= (1-init_dropout) ? Vertex(rand()) : missing
-random_edge(thr_mag=2) = Edge(0, rand()*thr_mag)
-random_res_graph(internal_size, external_size) = ResidualGraph([random_edge() for _ in 1:internal_size], [random_vertex() for _ in 1:internal_size, _ in 1:internal_size], [random_vertex() for _ in 1:internal_size, _ in 1:external_size], internal_size)
+random_vertex(init_dropout=0) = rand() >= init_dropout ? Vertex(rand()) : missing
+random_edge(thr_mag=2) = Edge(0, rand() * thr_mag)
+random_res_graph(internal_size, external_sizes) = ResidualGraph([random_edge() for _ in 1:internal_size], [random_vertex() for _ in 1:internal_size, _ in 1:internal_size], [[random_vertex() for _ in 1:internal_size, _ in 1:es_i] for es_i in external_sizes], internal_size)
 random_layer(input_size, output_size, res_connections) = Layer([random_edge() for _ in 1:input_size], [random_vertex() for _ in 1:input_size, _ in 1:output_size], [random_vertex() for _ in 1:input_size, _ in 1:res_connections], input_size)
 
 
-function update_value!(l::Layer, x)
+
+
+function update_edges!(l::Layer, x)
     for i in eachindex(l.edges, x)
         l.edges[i].value = x[i] + l.edges[i].b
     end
 end
-function update_value!(rg::ResidualGraph, x)
+function update_edges!(rg::ResidualGraph, x)
     for i in eachindex(rg.edges, x)
         rg.edges[i].value = x[i] + rg.edges[i].b
     end
 end
-function forward(l::Layer)
-    direct_out = zeros(axes(l.direct_vertices, 2))
-    res_out = zeros(axes(l.residual_vertices, 2))
 
-    for i in axes(l.direct_vertices, 1)
-        for j in axes(l.direct_vertecies, 2)
-            direct_out[j] += (l.edges[i].value * l.direct_verticies[i,j].w) / l.L
-        end
+import Base.*
+Base.*(e::Vector{Edge}, v::Array{Union{Vertex, Missing}, 2}) = [sum([e[i].value * v[i, j].w for i in skipmissing(v[:, j])]) for j in 1:length(v[1, :])]
+Base.*(x::Vector, v::Array{Union{Vertex, Missing}, 2}) = [sum([x[i] * v[i, j].w for i in skipmissing(v[:, j])]) for j in 1:length(v[1, :])]
 
-        for k in axes(l.residual_verticies, 2)
-            res_out[k] += (l.edges[i].value * l.residual_verticies[i,k].w) / l.L
-        end
+function forward!(layers::Array{Layer}, graph::ResidualGraph, x)
+
+    # TODO GraphStateBuffer = []
+
+    # 0. set edges initial state
+    graph.edges = graph.initial_edges
+    # TODO GraphStateBuffer[1] = graph.edges
+
+    for i in eachindex(layers)
+        # 1. get residual information for layer i from the graph
+        layer_suplement = graph.edges * graph.external_vertices[i]
+        # TODO V3-4: THIS CAN ALSO COLLECT RESIDUAL INFORMATION FOR EVERY LAYER AT THIS POINT
+
+        # 2. update layer edges from input and residual information
+        update_edges!(layers[i], x .+ layer_suplement)
+
+        # 3. calculate transmition of info from layer to res graph
+        res_in = layers[i].edges * layers[i].residual_vertices
+
+        # 4. calculate new internal graph state
+        update_edges!(graph, res_in * graph.internal_vertices)
+        # TODO GraphStateBuffer[i+1] = graph.edges
+
+        # 5. overwrite x
+        x = layers[i].edges * layers[i].direct_verticies
     end
-    return direct_out, res_out
-end
-function forward(rg::ResidualGraph)
-    internal_out = zeros(axes(rg.internal_verticies, 2))
-    external_out = zeros(axes(rg.external_verticies, 2))
 
-    for i in axes(rg.internal_verticies, 1)
-        for j in axes(rg.internal_verticies, 2)
-            internal_out[j] += (rg.edges[i].value * rg.internal_verticies[i,j].w) / rg.L
-        end
+    #=
+        (dE) d_x[L+1] / d_x[L] =
+            (dE) -> direct_verticies[L] + (dE) -> external_verticies[L] -> internal_vertecies[@ i=L-1 or i=init] -> residual_vertecies[L]
 
-        for k in axes(rg.external_verticies, 2)
-            external_out[k] += (rg.edges[i].value * rg.external_vertices[i,k].w) / rg.L
-        end
-    end
-    return direct_out, res_out
+        (dE) d_x[L+1] / d_w[L] =
+            (dE) -> x[L]
+
+        (dE) d_x[L+1] / d_b[L] =
+            (dE) -> 1
+
+        (dE) d_x[L+1] / d_residual_verticies[L] =
+            (dE) -> external_vertices[L] -> internal_vertices ->(outer) x[L]
+
+        (dE) d_x[L+1] / d_internal_verticies[L] =
+            (dE) -> external_vertices[L] ->(outer) (x[L] * residual_vertices[L])
+
+
+    =#
+
+
+    return x
 end
+
+
+
+
+
+
+
+
+
 
 
 using OpenAIGym
